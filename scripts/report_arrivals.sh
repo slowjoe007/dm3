042b8fe121bbf3e98659e248c4a9ada1245aa5c3
SlowJoe <slowjoe007@gmx.net>
2018-07-07 21:36:45 +0200

    ensure trailing slash is cropped

--- scripts/report_arrivals.sh
+++ scripts/report_arrivals.sh
@@ -6,9 +6,9 @@ inotifywait -mrq -e create --format %w%f "$ARRIVAL_DIR" | while read FILE
 do
     UUID=`uuidgen`
 
-    FILENAME=`basename $FILE`
+    FILENAME=`basename "$FILE"`
     #mv $FILE ${$ARRIVAL_DIR%/}   
-    logger "File \"${$ARRIVAL_DIR%/}/${UUID}_${FILENAME}\" has been created."
+    logger "File \"${ARRIVAL_DIR%/}/${UUID}_${FILENAME}\" has been created."
     
     # TODO - Trigger push notification
 done
