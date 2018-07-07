7716c540e286f6015f64e8bffe915993c6d39247
SlowJoe <slowjoe007@gmx.net>
2018-07-07 22:45:38 +0200

    instructions to add to rc3.d

--- scripts/report_arrivals.sh
+++ scripts/report_arrivals.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 # Requires `inotify-tools`, `uuid-runtime` to be installed
+# Register in '/etc/rc3.d' to be started on boot
+
+# Allow specifying the directory to monitor via command parameter
+if [ -n "$1" ]; then
+    ARRIVAL_DIR=$1
+fi
 
 inotifywait -mrq -e create --format %w%f "$ARRIVAL_DIR" | while read FILE
 do
